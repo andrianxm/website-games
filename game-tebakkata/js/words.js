42f80bbdd7c79bc20075847bf88265f401e682b4
itsyellow04 <143130038+yellowww04@users.noreply.github.com>
2024-01-12 05:48:51 +0700

    Add files via upload

---
+++ game-tebakkata/js/words.js
@@ -0,0 +1,198 @@
+let wordList = [
+    {
+        word: "phyton",
+        hint: "bahasa pemrograman"
+    },
+    {
+        word: "gitar",
+        hint: "alat musik dipetik"
+    },
+    {
+        word: "bidik",
+        hint: "saat kita memakai senjata"
+    },
+    {
+        word: "venus",
+        hint: "planet tata surya kita"
+    },
+    {
+        word: "emas",
+        hint: "logam mulia berwarna kuning paling langka"
+    },
+    {
+        word: "shopee",
+        hint: "situs belanja online berwarna orange"
+    },
+    {
+        word: "javascript",
+        hint: "bahasa pemrograman web"
+    },
+    {
+        word: "ngoding",
+        hint: "berkaitan dengan pemrograman"
+    },
+    {
+        word: "matriks",
+        hint: "materi yang banyak dialjabar"
+    },
+    {
+        word: "serangga",
+        hint: "hewan kecil-kecil"
+    },
+    {
+        word: "rokok",
+        hint: "menenangkan pikiran"
+    },
+    {
+        word: "gif",
+        hint: "format file untuk gambar bisa bergerak"
+    },
+    {
+        word: "mental",
+        hint: "berhubungan dengan pikiran"
+    },
+    {
+        word: "peta",
+        hint: "yang sering dikatakan di kartun dora"
+    },
+    {
+        word: "pulau",
+        hint: "tanah dikelilingi air"
+    },
+    {
+        word: "hoki",
+        hint: "datang keberuntungan"
+    },
+    {
+        word: "catur",
+        hint: "permainan yang dimainkan magnus carlsen"
+    },
+    {
+        word: "whatsapp",
+        hint: "aplikasi media sosial"
+    },
+    {
+        word: "github",
+        hint: "platform hosting kode"
+    },
+    {
+        word: "png",
+        hint: "format file gambar"
+    },
+    {
+        word: "perak",
+        hint: "logam berharga berwarna putih keabu-abuan"
+    },
+    {
+        word: "ponsel",
+        hint: "perangkat elektronik"
+    },
+    {
+        word: "gpu",
+        hint: "komponen komputer"
+    },
+    {
+        word: "jawa",
+        hint: "bahasa pemrograman"
+    },
+    {
+        word: "google",
+        hint: "mesin pencari terkenal"
+    },
+    {
+        word: "venesia",
+        hint: "kota perairan yang terkenal"
+    },
+    {
+        word: "unggul",
+        hint: "produk microsoft untuk windows"
+    },
+    {
+        word: "mysql",
+        hint: "sistem database relasional"
+    },
+    {
+        word: "nepal",
+        hint: "nama negara berkembang"
+    },
+    {
+        word: "seruling",
+        hint: "alat musik"
+    },
+    {
+        word: "kripto",
+        hint: "terkait dengan cryptocurrency"
+    },
+    {
+        word: "tesla",
+        hint: "satuan kerapatan fluks magnet"
+    },
+    {
+        word: "mars",
+        hint: "planet tata surya kita"
+    },
+    {
+        word: "proksi",
+        hint: "terkait dengan aplikasi server"
+    },
+    {
+        word: "email",
+        hint: "terkait dengan pertukaran pesan"
+    },
+    {
+        word: "html",
+        hint: "bahasa markup untuk web"
+    },
+    {
+        kata: "udara",
+        hint: "berhubungan dengan gas"
+    },
+    {
+        word: "ide",
+        hint: "sebuah pemikiran atau saran"
+    },
+    {
+        word: "pelayan",
+        hint: "berkaitan dengan komputer atau sistem"
+    },
+    {
+        word: "svg",
+        hint: "format gambar vektor"
+    },
+    {
+        kata: "jpeg",
+        hint: "format file gambar"
+    },
+    {
+        word:"pencarian",
+        hint: "bertindak untuk menemukan sesuatu"
+    },
+    {
+        word: "kunci",
+        hint: "sepotong kecil logam"
+    },
+    {
+        word: "Mesir",
+        hint: "nama negara"
+    },
+    {
+        word: "pelawak",
+        hint: "film thriller psikologis"
+    },
+    {
+        word: "dubai",
+        hint: "nama negara maju"
+    },
+    {
+        word: "foto",
+        hint: "representasi orang atau pemandangan"
+    },
+    {
+        word: "sungai",
+        hint: "sungai terbesar di dunia"
+    },
+    {
+        word: "hujan",
+        hint: "berhubungan dengan air"
+    },
+]
